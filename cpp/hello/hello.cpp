5e55d215455cc3d84845da35e331050202a52294
Morgan Benton <morgan.benton@gmail.com>
2018-03-16 10:48:55 -0400

    added c++ hello world

---
+++ cpp/hello/hello.cpp
@@ -0,0 +1,20 @@
+// Simple C++ program to display "Hello World"
+
+// NOTE: To compile on Windows you need
+// to add an additional flag to tell the
+// compiler which subsystem to use. see:
+// https://stackoverflow.com/a/5260237/296725
+// 
+// $ g++ hello.cpp -o hello -mwindows
+
+// Header file for input output functions
+#include<iostream> 
+ 
+using namespace std;
+ 
+// main function -
+// where the execution of program begins
+int main() {
+    cout << "Hello, World!";   
+    return 0;
+}
