2a97deda340c7052ea92bc51c96c0d173a3268aa
Morgan Benton <morgan.benton@gmail.com>
2018-04-02 09:58:06 -0400

    starting fizzbuzz in python

---
+++ python/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,13 @@
+# fizzbuzz.py
+
+class FizzBuzz:
+  x = "hello"
+
+  # def fizz(self, x):
+  #   return "Fizz" if x % 3 == 0 else x
+
+  fizz = lambda self, x: "Fizz" if x % 3 == 0 else x
+  buzz = lambda self, x: "Buzz" if x % 5 == 0 else x
+
+  # def buzz(self, x):
+  #   return "Buzz" if x % 5 == 0 else x
